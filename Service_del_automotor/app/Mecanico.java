fd108efd2659d21028cf441955b3855fff644603
Margarita-Bravo <bravo_m33@hotmail.com>
2023-09-28 12:33:50 -0300

    Activity mecanico

---
+++ Service_del_automotor/app/Mecanico.java
@@ -0,0 +1,2 @@
+public class Mecanico {
+}
--- Service_del_automotor/app/src/main/java/com/example/service_del_automotor/Mecanico.java
+++ Service_del_automotor/app/src/main/java/com/example/service_del_automotor/Mecanico.java
@@ -1,6 +1,13 @@
 package com.example.service_del_automotor;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Mecanico extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mecanico);
+    }
 }
