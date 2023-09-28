22d4c6355953c0ad946f5fcea6c9a5da89035c89
TamiSorsaburu <tamysorsaburu@gmail.com>
2023-09-28 17:12:08 -0300

    Se agrega navegabilidad a la activity presentacion

--- Service_del_automotor/app/src/main/java/com/example/service_del_automotor/MainActivity.java
+++ Service_del_automotor/app/src/main/java/com/example/service_del_automotor/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.service_del_automotor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonLogin(View view) {
+        Intent intent = new Intent(this, IniciarSesion.class);
+        startActivity(intent);
+    }
+
+    public void textRegister(View view) {
+        Intent intent = new Intent(this, Registrarse.class);
+        startActivity(intent);
     }
 }
