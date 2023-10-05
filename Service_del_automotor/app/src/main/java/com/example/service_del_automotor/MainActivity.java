2de0bd407cf33a1b80104e7a76c9274ff8bda8b4
Margarita-Bravo <bravo_m33@hotmail.com>
2023-10-05 17:24:25 -0300

    cargando bd

--- Service_del_automotor/app/src/main/java/com/example/service_del_automotor/MainActivity.java
+++ Service_del_automotor/app/src/main/java/com/example/service_del_automotor/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ConexionSQLiteHelper conn=new ConexionSQLiteHelper(this,"bd_cliente",null,1);
+        ConexionSQLiteHelper conn=new ConexionSQLiteHelper(this,"servicioAutomotor1",null,1);
 
         SQLiteDatabase db = conn.getWritableDatabase();
     }
